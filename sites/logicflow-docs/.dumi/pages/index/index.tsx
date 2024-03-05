7e5ef26ed76fcb6da58643e9a01abd7761cdc0ed
zhangmo <zhangmo@didiglobal.com>
2024-03-05 14:38:18 +0800

    fix: 首页加载时长

--- sites/logicflow-docs/.dumi/pages/index/index.tsx
+++ sites/logicflow-docs/.dumi/pages/index/index.tsx
@@ -1,58 +1,20 @@
 /**
  * title: LogicFlow
  */
-import React from 'react';
-import Introduction from './components/Introduction';
-import Demo from './components/demo/index';
-import MobileDemo from './components/demo2/index';
-import Animation from './components/animation';
-import './index.less';
+import React, { Suspense, lazy } from 'react';
 
-const Homepage: React.FC = () => {
-  // const scrollDown = () => {
-  //   window.scrollTo(0, 650);
-  // };
+const Inner = lazy(() => import('./inner'));
 
-  const isMobile = () => {
-    let flag =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent,
-      );
-    return flag;
-  };
-
+const Indexpage: React.FC = () => {
   return (
-    <section className="home-page">
-      <div className="home-title">
-        {/* 名称 */}
-        <div className="dumi-default-hero">
-          <h1 className="dumi-default-hero-title left-title">
-            <span
-              style={{
-                background: 'linear-gradient(30deg, #3eaefd 30%, #2b65fa)',
-                backgroundClip: 'revert-layer',
-              }}
-            >
-              LogicFlow
-            </span>
-          </h1>
-          <p>低成本实现，让逻辑管理更简单、更高效</p>
-          <div className="dumi-default-hero-actions">
-            <a href="/tutorial">开始使用</a>
-            <a href="https://github.com/didi/LogicFlow" target="_blank">
-              Github
-            </a>
-          </div>
-          {/* <span className="more-icon" onClick={scrollDown}></span> */}
-        </div>
-        <Animation />
-      </div>
-      {/* 首页 demo */}
-      {isMobile() ? <MobileDemo /> : <Demo />}
-      {/* 介绍 */}
-      <Introduction />
-    </section>
+    <Suspense
+      fallback={
+        <div style={{ marginTop: '30vh', textAlign: 'center' }}>Loading...</div>
+      }
+    >
+      <Inner />
+    </Suspense>
   );
 };
 
-export default Homepage;
+export default Indexpage;
