310f74c5844017771308ad0fa9180f9b8912b6af
zhangmo <zhangmo@didiglobal.com>
2024-03-19 11:49:32 +0800

    fix: 实例位置修改成嵌入式

--- sites/logicflow-docs/.dumirc.ts
+++ sites/logicflow-docs/.dumirc.ts
@@ -16,19 +16,13 @@ export default defineConfig({
       'zh-CN': [
         { title: '文档', link: '/tutorial' },
         { title: 'API', link: '/api' },
-        {
-          title: '示例',
-          link: 'https://examples.logic-flow.cn',
-        },
+        { title: '示例', link: '/examples' },
         { title: '文章', link: '/article/article01' },
       ],
       'en-US': [
         { title: 'Tutorial', link: '/en-US/tutorial' },
         { title: 'API', link: '/en-US/api' },
-        {
-          title: 'Examples',
-          link: 'https://examples.logic-flow.cn',
-        },
+        { title: 'Examples', link: '/en-US/examples' },
         { title: 'Article', link: '/en-US/article/article01' },
       ],
     },
